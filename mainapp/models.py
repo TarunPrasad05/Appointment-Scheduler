e1fbf633d7f7b88925914035bcf6ada22f3dab79
TarunPrasad05 <tarunprasad789@gmail.com>
2023-04-28 22:24:02 +0530

    Appointment booking implementation basic level

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+TIME_CHOICES = (
+    ("10 AM", "10 AM"),
+    ("11 AM", "11 AM"),
+    ("12 PM", "12 PM"),
+    ("1 PM", "1 PM"),
+    ("2 PM", "2 PM"),
+    ("3 PM", "3 PM"),
+    ("4 PM", "4 PM"),
+    ("5 PM", "5 PM"),
+    ("6 PM", "6 PM"),
+    ("7 PM", "7 PM"),
+)
+
+class Appointment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    user2 = models.CharField(User, null=True, max_length=30)
+    day = models.DateField(default=datetime.now)
+    time = models.CharField(max_length=10, choices=TIME_CHOICES, default="3 PM")
+    time_ordered = models.DateTimeField(default=datetime.now, blank=True)
+    def __str__(self):
+        return f"{self.user.username} | day: {self.day} | time: {self.time}"
