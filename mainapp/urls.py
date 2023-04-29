4639712fd8e381796e97fb3b3e00b031074ab7ae
TarunPrasad05 <tarunprasad789@gmail.com>
2023-04-29 10:46:10 +0530

    Appointment booking changes

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -14,16 +14,8 @@ urlpatterns = [
 
     #user registration
     path('register/', views.register, name='register'),
-
-    #appointment booking
-    path('book_appointment/', views.book_appointment, name="book_appointment"),
     
     #new
-    path('', views.index, name='index'),
     path('booking', views.booking, name='booking'),
     path('booking-submit', views.bookingSubmit, name='bookingSubmit'),
-    path('user-panel', views.userPanel, name='userPanel'),
-    path('user-update/<int:id>', views.userUpdate, name='userUpdate'),
-    path('user-update-submit/<int:id>', views.userUpdateSubmit, name='userUpdateSubmit'),
-    path('staff-panel', views.staffPanel, name='staffPanel'),
 ]
